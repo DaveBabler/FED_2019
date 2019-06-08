73b6b3c9963edac95aaf06b59433b81144f1319d
Dave Babler <dababler@outlook.com>
2019-06-08 17:03:55 -0400

    testing includes

--- config.php
+++ config.php
@@ -1,6 +1,13 @@
 <?php
-define('ROOT_PATH', dirname(__DIR__).'/');
+session_start();
+define('ROOT_PATH', dirname(__DIR__).'/FED_2020/');
+define('PRIV_PATH', ROOT_PATH.'PULL_OUT_TO_SERVER/'); //WHERE LOGINS WILL GO be sure to replace that last concat!!!
+include(PRIV_PATH.'db.php');
+// $included_files = get_included_files();
 
-echo ROOT_PATH;
+// foreach ($included_files as $filename) {
+//     echo "$filename\n";
+// }
+
 ?>
 
