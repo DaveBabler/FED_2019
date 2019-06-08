dbc329d961cb3dbba4bd1c89c4e17776742d0c90
Dave Babler <dababler@outlook.com>
2019-06-08 17:21:40 -0400

    testing configuration

--- config.php
+++ config.php
@@ -3,11 +3,14 @@ session_start();
 define('ROOT_PATH', dirname(__DIR__).'/FED_2020/');
 define('PRIV_PATH', ROOT_PATH.'PULL_OUT_TO_SERVER/'); //WHERE LOGINS WILL GO be sure to replace that last concat!!!
 include(PRIV_PATH.'db.php');
-// $included_files = get_included_files();
+/*  $included_files = get_included_files();
 
-// foreach ($included_files as $filename) {
-//     echo "$filename\n";
-// }
+ foreach ($included_files as $filename) {
+     echo $filename."<br>";
+     echo "<hr>";
+ }
+ */
+
 
 ?>
 
