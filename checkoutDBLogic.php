7200df607c7b11462974fa720d24754fc0428bd0
Dave Babler <dababler@outlook.com>
2019-06-12 21:51:28 -0400

    add proper function page call

--- checkoutDBLogic.php
+++ checkoutDBLogic.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include('/home/dbabler/dbabler.yaacotu.com/FED_2020/PULL_OUT_TO_SERVER/db.php');
-include('function.php');
+include('/home/dbabler/dbabler.yaacotu.com/FED_2020/Scripts/PHP/function.php');
 $queryType = $_POST['queryType'][0];
 unset($cartItem);
 
